d72d159ecad075309e96c8f0bf3cdc7c4d2d75d5
Abdo Elbessa <zezoelbessa10@egyeagles.com>
2024-01-08 20:11:48 +0200

    Add project files.

---
+++ Singelton design pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Singelton_design_pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singelton s1 = Singelton.getInstatnt();
+            Singelton s2 = Singelton.getInstatnt();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("SingelTon Work");
+            }
+            else
+            {
+                Console.WriteLine("Oopss Singelton dosnt work");
+            }
+
+
+        }
+    }
+}
---
+++ Singelton design pattern/Singelton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singelton_design_pattern
+{
+    internal class Singelton
+    {
+        private static Singelton _instatnt;
+        private Singelton()
+        {
+            
+        }
+        public static Singelton getInstatnt()
+        {
+            if (_instatnt == null)
+            {
+                _instatnt = new Singelton();
+            }
+            return _instatnt;
+        }
+    }
+}
